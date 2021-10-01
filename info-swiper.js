fb8098a6b0f1341663eb690396787e15a776af1c
mixmaxmix <62526387+mixmaxmix@users.noreply.github.com>
2021-10-01 13:18:06 +0300

    Many adaptive fixes and slider fix

--- info-swiper.js
+++ info-swiper.js
@@ -1,11 +1,10 @@
+
 const swiper = new Swiper('.review-swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     spaceBetween: 50,
-
+    
     slidesPerView: 3,
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -25,18 +24,42 @@ const swiper = new Swiper('.review-swiper', {
 });
 
 const skiper = new Swiper('.info-slider', {
-    // Optional parameters
-    // direction: 'horizontal',
     spaceBetween: 50,
+    allowTouchMove: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
-      },
-
-    slidesPerView: 1,
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
     },
+    
+    slidesPerView: 1,
 });
+
+let sliderCounter = 1;
+let allSlides = document.body.querySelectorAll('.slide');
+let sliderClick = document.body.querySelector('.more-info-slider').addEventListener('transitionend', checkSlide);
+const nextSlide = document.body.querySelector('.swiper-button-next');
+const prevSlide = document.body.querySelector('.swiper-button-prev');
+
+function checkSlide() {
+    if (allSlides[0].classList.contains('swiper-slide-active')) {
+        sliderCounter = 1;
+        document.body.querySelector('.number-of-slide').innerHTML = `${sliderCounter}`;
+        document.body.querySelector('.text-of-slide').innerHTML = `${slideTitles[sliderCounter - 1]}`
+    } else if (allSlides[1].classList.contains('swiper-slide-active')) {
+        sliderCounter = 2;
+        document.body.querySelector('.number-of-slide').innerHTML = `${sliderCounter}`;
+        document.body.querySelector('.text-of-slide').innerHTML = `${slideTitles[sliderCounter - 1]}`
+    } else if (allSlides[2].classList.contains('swiper-slide-active')) {
+        sliderCounter = 3;
+        document.body.querySelector('.number-of-slide').innerHTML = `${sliderCounter}`;
+        document.body.querySelector('.text-of-slide').innerHTML = `${slideTitles[sliderCounter - 1]}`
+    }
+}
+
+
+
+// Массив слайдов, которые нужно менять
+const slideTitles = ['Изучаете модули в нашей системе', 'Слайд номер 2', 'Слайд номер 3']
+
+
+document.body.querySelector('.text-of-slide').innerHTML = `${slideTitles[sliderCounter - 1]}`
